b32299bb77bad8845b075d3c212c6b9e1244a8e9
Emily Abrash <emabrash@gmail.com>
2016-10-04 16:37:23 -0700

    Read in CSVs for all four database sections

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+CSV.foreach('seed_csvs/markets.csv', :headers => false) do |csv_obj|
+  Market.create(id: csv_obj[0].to_i, name: csv_obj[1], address: csv_obj[2], city: csv_obj[3], county: csv_obj[4], state: csv_obj[5], zip: csv_obj[6])
+end
+
+CSV.foreach('seed_csvs/vendors.csv', :headers => false) do |csv_obj|
+  Vendor.create(id: csv_obj[0].to_i, name: csv_obj[1], num_employees: csv_obj[2].to_i, market_id: csv_obj[3])
+end
+
+CSV.foreach('seed_csvs/sales.csv', :headers => false) do |csv_obj|
+  Sale.create(id: csv_obj[0].to_i, amount: csv_obj[1].to_i, purchase_time: DateTime.parse(csv_obj[2]), vendor_id: csv_obj[3].to_i, product_id: csv_obj[4].to_i)
+end
+
+CSV.foreach('seed_csvs/products.csv', :headers => false) do |csv_obj|
+  Product.create(id: csv_obj[0].to_i, name: csv_obj[1], vendor_id: csv_obj[2].to_i)
+end
