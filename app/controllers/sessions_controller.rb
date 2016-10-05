2a6029e32f1c20100f80ac14eb3492a1f6cd10c7
Brandi Phillips <milligan.brandi@gmail.com>
2016-10-05 14:54:14 -0700

    session creation working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,19 @@
 class SessionsController < ApplicationController
   def index
-    case :user_type
-      when 'user'
-        redirect_to users_search_path
-      when 'market'
-        redirect_to create_session_path
-      when 'vendor'
-        redirect_to create_session_path
-    end
+
   end
 
   def new
+    @user = params[:user_type]
   end
 
   def create
-    user = {type: params[:session][:type], id: params[:session][:id]}
-    log_in (user)
-    if params[:session][:type].downcase == "market"
+    @user = params[:user_type]
+    # user = {type: @user, id: params[:session][:id]}
+    # log_in (user)
+    if @user.downcase == "market"
       redirect_to markets_index_path
-    elsif params[:session][:type].downcase == "vendor"
+    elsif @user.downcase == "vendor"
       redirect_to vendors_index_path
     else
       redirect_to users_index_path
