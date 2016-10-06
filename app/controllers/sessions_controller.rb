ace5c28f7aa48dbdbeb96fbad1aafc080952c4af
Emily Abrash <emabrash@gmail.com>
2016-10-06 14:04:41 -0700

    Added some dropdowns, DRYed up market forms, small edits to login mechanism

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,31 @@
 class SessionsController < ApplicationController
   def index
-    case :user_type
-      when 'user'
-        redirect_to users_search_path
-      when 'market'
-        redirect_to create_session_path
-      when 'vendor'
-        redirect_to create_session_path
-    end
+
   end
 
   def new
+    if params[:user_type] == 'user'
+      user = {type: params[:user_type], id: nil}
+      log_in (user)
+      puts current_user.id
+      redirect_to users_index_path
+    end
+
+    @type = params[:user_type]
   end
 
   def create
-    user = {type: params[:session][:type], id: params[:session][:id]}
+
+    user = {type: params[:user_type], id: params[:session][:id]}
     log_in (user)
-    if params[:session][:type].downcase == "market"
+    if params[:user_type].downcase == "market"
       redirect_to markets_index_path
-    elsif params[:session][:type].downcase == "vendor"
+    elsif params[:user_type].downcase == "vendor"
       redirect_to vendors_index_path
     else
       redirect_to users_index_path
     end
+
   end
 
   def destroy
